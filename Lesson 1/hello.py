85de108e4c4a75f21d7585e07cc868a46aed1eb2
Adefreit <Adefreit@users.noreply.github.com>
2019-08-09 13:08:13 -0600

    Uploading Lesson 1 Example

---
+++ Lesson 1/hello.py
@@ -0,0 +1,13 @@
+'''
+YOUR FIRST PYTHON PROGRAM
+In computer science, we have a tradition where we
+introduce students to a language by showing them how
+to print "Hello World".
+
+BTW, everything written here is called a comment.
+Comments are sections of text that we write to explain
+things to ourselves (or other programmers).  The computer
+ignores these lines, and only executes the code (below).
+'''
+
+print("hello world!")
