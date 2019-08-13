9dad89af53a38648c5792df5fb7db33e11f66b3e
Adrian de Freitas <adrian.defreitas@gmail.com>
2019-08-13 00:08:19 -0600

    Adding Lesson 2

---
+++ Lesson 2/io_examples.py
@@ -0,0 +1,18 @@
+# Printing Stuff
+#print("hello minions!")
+#print("hello", "minions!")
+#print("hello\nworld")
+
+# Assigning Variables
+#name = 'Adrian'
+#print(name)
+#print('hello', name)
+#print('hello', 'name')
+
+# Input
+#age = int(input('What is your age? '))
+#print('You will graduate when you are', age + 4, 'years old')
+
+
+
+
