1c9cf17d7c469751850a4c43b8510ac5e6b476c8
Adefreit <Adefreit@users.noreply.github.com>
2019-08-15 12:03:49 -0600

    Uploading Lesson 3

---
+++ Lesson 3/lesson3-challenges.py
@@ -0,0 +1,31 @@
+# Pre Class Challenge #1
+#print('Good Morning', "Class")
+
+# Pre Class Challenge #2
+#print(      'Good Morning')
+
+# Pre Class Challenge #3
+#x = 5
+#y = 4
+#print("x + y")
+
+# Pre Class Challenge #4
+#name1 = input()
+#print("Good Morning", name1)
+
+# Pre Class Challenge #5
+#print('ready') print('to') print("learn")
+
+# Math Examples
+#print("The absolute value of -3.2 is", abs(-3.2))
+#print("20.23 rounded to one decimal is", round(20.23, 1))
+#print("The square root of 1024 is", math.sqrt(1024))
+
+# Formatted Print
+#name = "de Freitas"
+#pft_score = 301
+#aft_score = 250
+#physical_education_average = 2.3
+
+#print("Cadet %s received a %d on the PFT, and a %d on the AFT, giving him an overall PEA of %0.3f"
+#      % (name, pft_score, aft_score, physical_education_average))
---
+++ Lesson 3/lesson3-ipo.py
@@ -0,0 +1,13 @@
+# Example Input-Process-Output Problem
+# Problem:  Write an algorithm that gets two numbers from the
+# user, calculates the average, and outputs the result.
+
+# Step 1:  Get the values
+value1 = int(input())
+value2 = int(input())
+
+# Step 2:  Process the values
+average = (value1 + value2) / 2
+
+# Step 3:  Print the Result
+print(average)
