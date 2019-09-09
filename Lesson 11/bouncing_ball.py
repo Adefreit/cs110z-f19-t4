94ae6c3e999bb925011f914366a188cfeb02e136
Adrian de Freitas <adrian.defreitas@gmail.com>
2019-09-09 00:19:33 -0600

    Lesson 11 Added

---
+++ Lesson 11/bouncing_ball.py
@@ -0,0 +1,22 @@
+import pythonGraph
+
+# Global Variables
+ball_x = 300
+ball_y = 300
+ball_x_velocity = 3
+ball_y_velocity = 5
+
+# This Opens the Graph Window
+pythonGraph.open_window(600, 600)
+
+while pythonGraph.window_not_closed():
+    # Step 1: Erase Everything on the Screen
+            
+    # Step 2: Move the Ball
+    
+    # Step 3: Draw the Ball
+            
+    # Step 4: Update the Screen 
+    pythonGraph.update_window()
+
+
---
+++ Lesson 11/return_practice.py
@@ -0,0 +1,13 @@
+import math
+
+# This version of the distance formula just calculates the answer
+def distance_formula_v1(x1, y1, x2, y2):
+    distance = math.sqrt((x2-x1)**2 + (y2-y1)**2)
+
+# This version of the distance formula RETURNS the answer
+def distance_formula_v2(x1, y1, x2, y2):
+    distance = math.sqrt((x2-x1)**2 + (y2-y1)**2)
+    return distance
+
+print(distance_formula_v2(0, 0, 10, 0))
+    
